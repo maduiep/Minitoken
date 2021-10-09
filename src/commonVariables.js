b44950847563b8bdea687feaabe6a66a45bc2ce7
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-10-09 15:48:48 +0100

    updated common variable file

--- src/commonVariables.js
+++ src/commonVariables.js
@@ -1,6 +1,6 @@
 const currentUrl = window.location.href;
 export const BASE_URL = 
 currentUrl.includes('ngrok') ? 
-    "https://4a12-197-210-55-239.ngrok.io" : 
+    "https://minitoken.herokuapp.com/" : 
     'http://localhost:3000';
 
