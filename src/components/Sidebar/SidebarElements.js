f45aa1be2f01d7605b7976c8dd347d0165667f02
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-10-09 16:50:05 +0100

    fied sidebar error

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -8,7 +8,7 @@ export const SidebarContainer = styled.aside`
     position: fixed;
     z-index: 1000;
     display: grid;
-    width: 100%;
+    width: 80%;
     height: 100%;
     align-items: center;
     background: var(--primary-color);
@@ -68,6 +68,9 @@ export const SidebarLink = styled(LinkScroll)`
 
     @media screen and (max-width: 680px) {
         font-size: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
 
         &:hover {
         transition: all 0.2s ease-in-out;
@@ -79,6 +82,7 @@ export const SidebarLink = styled(LinkScroll)`
 export const SidebarBtnWrap = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
 `
 
 export const SidebarRoute = styled(LinkRouter)`
